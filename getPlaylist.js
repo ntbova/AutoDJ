c422181cb24e3ef03f5f0a4f4cc94f748c75e395
craigdunton <craigodawg19@gmail.com>
2019-01-22 16:30:46 -0500

    show error message for other topics

--- getPlaylist.js
+++ getPlaylist.js
@@ -3,11 +3,17 @@ const sources = {
   'new york': 'https://open.spotify.com/embed/user/1224963539/playlist/3pS0p7Lrqu4DFMpFkKhUVp',
   'places': 'https://open.spotify.com/embed/user/1224963539/playlist/36DfTg9bztkYD1DN91N3tk',
 }
+
 window.onload = function() {
   document.getElementById('setTopicButton').onclick = function() {
     const topic = document.getElementById('topicName').value;
     document.getElementById('topicName').value = '';
+    
     const iframeSrc = sources[topic.toLowerCase()];
-    document.getElementById('player-frame').src = iframeSrc;
+    if(iframeSrc === undefined) {
+      document.getElementById('spotify-player').innerHTML = '<h4>Unable to create a playlist for that topic. Sorry!</h4>'
+    } else {
+      document.getElementById('spotify-player').innerHTML = '<iframe id="player-frame" src='+iframeSrc+' width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>';
+    }
   }
 }
