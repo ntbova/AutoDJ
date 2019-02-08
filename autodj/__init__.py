751b54bbb98dbb0f8b830084d361f62884d366a6
Stevever <stevensun96@hotmail.com>
2019-02-08 02:07:45 -0500

    Set up routing for a simple Hello World page

---
+++ autodj/__init__.py
@@ -0,0 +1,27 @@
+import os
+
+from flask import Flask, render_template
+
+
+def create_app(test_config=None):
+    # create the app
+    app = Flask(__name__, instance_relative_config=True)
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed on
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route('/')
+    def index():
+        return 'Hello, World!'
+
+    return app
---
+++ run.py
@@ -0,0 +1,5 @@
+from autodj import create_app
+
+# run a test server
+if __name__ == '__main__':
+    create_app().run()
