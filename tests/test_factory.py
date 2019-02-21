54c7400fe3292248f1bcb6cc1e7799f40c83424c
Nick Bova <ntbova@gmail.com>
2019-02-21 16:58:05 -0500

    Added requests module to requirements.txt

--- tests/test_factory.py
+++ tests/test_factory.py
@@ -4,6 +4,10 @@ def test_config():
     assert not create_app().testing
     assert create_app({'TESTING': True}).testing
 
+def test_login(client):
+    response = client.get('/login')
+    assert response
+
 
 # def test_hello(client):
 #     response = client.get('/hello')
