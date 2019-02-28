4caa12d1189427ad6b222dac95f16f0e1833f909
Matthew Stock <mattestevanstock@gmail.com>
2019-02-28 16:44:15 -0500

    Added slider which filters results based on song's sentiment score.

--- autodj/static/getPlaylist.js
+++ autodj/static/getPlaylist.js
@@ -1,12 +1,28 @@
 window.onload = function() {
+  
+  var moods = ['Negative', 'Neutral', 'Positive'];
+
+  var output = 1;
+  document.getElementById('myRange').oninput = function() {
+    output = this.value;
+
+    document.getElementById('sentiment').innerText = moods[output];
+  }
+
   document.getElementById('setTopicButton').onclick = function() {
     const topic = document.getElementById('topicName').value;
     document.getElementById('topicName').value = '';
+    
+    if (topic === '') {
+      return
+    }
 
     document.getElementById('playlist-text').innerText = 'Creating a playlist...'
 
+    var fullURL = "https://gateway-wdc.watsonplatform.net/discovery/api/v1/environments/4b24f2d8-d802-4b28-bec2-bc4104ebb8b4/collections/60f87acf-22e1-4677-aae7-23645d3beccd/query?version=2018-12-03&filter=enriched_lyrics.sentiment.document.label%3A%3A\"" + moods[output].toLowerCase() + "\"&query=enriched_lyrics.concepts.text%3A%22" + encodeURI(topic) + "%22%7Clyrics%3A%22" + encodeURI(topic) + "%22";
+
     $.ajax({
-      url: "https://gateway-wdc.watsonplatform.net/discovery/api/v1/environments/4b24f2d8-d802-4b28-bec2-bc4104ebb8b4/collections/60f87acf-22e1-4677-aae7-23645d3beccd/query?version=2018-12-03&query=enriched_lyrics.concepts.text%3A%22" + encodeURI(topic) + "%22%7Clyrics%3A%22" + encodeURI(topic) + "%22",
+      url: fullURL,
       beforeSend: function(xhr) {
         xhr.setRequestHeader("Authorization", "Basic " + btoa("apikey:ltytYzYR-49NwvrxbnIDILPe_9fUqOn86MMEIYDhdgHB"));
       },
