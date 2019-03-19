629b594cbc86b37e9924dad3cd910291ea0930ef
craigdunton <craigodawg19@gmail.com>
2019-03-19 17:12:41 -0400

    rebased on master

--- extension/getPlaylist.js
+++ extension/getPlaylist.js
@@ -3,12 +3,21 @@ window.onload = function() {
   var moods = ['Negative', 'Neutral', 'Positive'];
 
   var output = 1;
+  var activated = false;
+
+  // Function for slider changes.
   document.getElementById('myRange').oninput = function() {
     output = this.value;
-    console.log('output: ' + output);
+
     document.getElementById('sentiment').innerText = moods[output];
   }
 
+  // Function for slider checkbox changes.
+  document.getElementById('sliderBox').onclick = function() {
+    activated = this.checked;
+  }
+
+  // Function for button which uses Discovery query based on input text.
   document.getElementById('setTopicButton').onclick = function() {
     const topic = document.getElementById('topicName').value;
     document.getElementById('topicName').value = '';
@@ -19,8 +28,12 @@ window.onload = function() {
 
     document.getElementById('playlist-text').innerText = 'Creating a playlist...'
 
-    var fullURL = "https://gateway-wdc.watsonplatform.net/discovery/api/v1/environments/4b24f2d8-d802-4b28-bec2-bc4104ebb8b4/collections/60f87acf-22e1-4677-aae7-23645d3beccd/query?version=2018-12-03&filter=enriched_lyrics.sentiment.document.label%3A%3A\"" + moods[output].toLowerCase() + "\"&query=enriched_lyrics.concepts.text%3A%22" + encodeURI(topic) + "%22%7Clyrics%3A%22" + encodeURI(topic) + "%22";
-    console.log("Sending request url: " + fullURL);
+    if (activated) {
+      var fullURL = "https://gateway-wdc.watsonplatform.net/discovery/api/v1/environments/4b24f2d8-d802-4b28-bec2-bc4104ebb8b4/collections/60f87acf-22e1-4677-aae7-23645d3beccd/query?version=2018-12-03&filter=enriched_lyrics.sentiment.document.label%3A%3A\"" + moods[output].toLowerCase() + "\"&query=enriched_lyrics.concepts.text%3A%22" + encodeURI(topic) + "%22%7Clyrics%3A%22" + encodeURI(topic) + "%22";
+    } else {
+      var fullURL = "https://gateway-wdc.watsonplatform.net/discovery/api/v1/environments/4b24f2d8-d802-4b28-bec2-bc4104ebb8b4/collections/60f87acf-22e1-4677-aae7-23645d3beccd/query?version=2018-12-03&query=enriched_lyrics.concepts.text%3A%22" + encodeURI(topic) + "%22%7Clyrics%3A%22" + encodeURI(topic) + "%22";
+    } 
+
     $.ajax({
       url: fullURL,
       beforeSend: function(xhr) {
@@ -30,7 +43,6 @@ window.onload = function() {
       success: function (data) {
         // console.log(JSON.stringify(data)); data.results: [{ song, artist, lyrics, other watson stuff }]
         // const songs = data.results.slice(0, 9);
-        console.log(JSON.stringify(data));
         const songs = data.results;
 
         if (Object.keys(songs).length == 0) {
