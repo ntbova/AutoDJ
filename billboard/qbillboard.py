7f48d95de21bfd39a631dd8f77725d170262bf03
Nick Bova <ntbova@gmail.com>
2019-03-21 16:18:46 -0400

    Added additional songs to upload to Discovery

--- billboard/qbillboard.py
+++ billboard/qbillboard.py
@@ -8,6 +8,6 @@ with open('data/billboard_data', 'a') as outfile:
     for song in chart:
         song_dict = {'song': song.title, 'artist': song.artist}
         song_str = json.dump(song_dict, outfile)
-        outfile.write('\n')
+        outfile.write(',\n')
 
 # print(song_file_name)
